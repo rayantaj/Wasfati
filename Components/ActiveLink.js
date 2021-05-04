aafeff188545fc2358f3400ba982660184904835
rayantaj <50342275+rayantaj@users.noreply.github.com>
2021-05-04 06:51:29 +0300

    designing the disktop side bar and adding icons

---
+++ Components/ActiveLink.js
@@ -0,0 +1,101 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Box, Flex, Text, Icon } from "@chakra-ui/react";
+import { BsEggFried } from "react-icons/bs";
+/**
+ * this file was built to add active link functionality above Next Link component
+ */
+
+// for primary Navigation
+const PrimaryLink = ({ href, children, icon }) => {
+    const { asPath } = useRouter();
+    let color = "black";
+    let bg;
+    let hover = true;
+    let fontWeight = "normal";
+    if (asPath === href) {
+        color = "white";
+        bg = "black";
+        hover = false;
+        fontWeight = "bold";
+    }
+    return (
+        <Box borderRadius="3xl" bgColor={bg} p="0.5rem" color={color}
+            fontWeight={fontWeight}>
+            <Flex align="center">
+
+                <Icon as={icon} color={color}></Icon>
+                <Box ml="0.5rem">
+                    <Link href={href}>{React.cloneElement(children)}
+
+                    </Link>
+                </Box>
+
+
+
+
+            </Flex>
+
+
+        </Box>
+        // {/* <Box
+        //     color={color}
+        //     fontWeight={"bold"}
+        //     _hover={{
+        //         color: "gray.400",
+        //     }}
+        // >
+        //     <Link href={href}>{React.cloneElement(children)}</Link>
+        // </Box> */}
+    );
+};
+
+// for primary Navigation
+const SecondaryLink = ({ href, children }) => {
+    const { asPath } = useRouter();
+
+    let color = "black";
+    let bg;
+    let hover = true;
+    if (asPath === href) {
+        color = "primary.dark";
+        bg = "primary.light";
+        hover = false;
+    }
+
+    return (
+        <Box
+            fontWeight={color && "medium"}
+            _hover={
+                hover && {
+                    bg: "gray.100",
+                }
+            }
+            borderStart={!hover && "2px"}
+            borderColor={!hover && color}
+            color={color}
+            bg={bg}
+            p={3}
+        >
+            <Link href={href}>{React.cloneElement(children)}</Link>
+        </Box>
+    );
+};
+
+// parent component
+const ActiveLink = ({ type, ...props }) => {
+    const links = {
+        primary: PrimaryLink,
+
+    };
+
+    if (!type) {
+        return null;
+    }
+
+    const Component = links[type];
+    return <Component {...props} />;
+};
+
+export default ActiveLink;
--- Layout/DisktopNav.js
+++ Layout/DisktopNav.js
@@ -1,10 +1,141 @@
 import React from 'react'
 import { Heading, Box, Center, Text, Link, Divider, Flex, Spacer, Icon } from "@chakra-ui/react"
-
+import { BsEggFried } from "react-icons/bs";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { GiChickenOven } from "react-icons/gi";
+import { GrToast } from "react-icons/gr";
+import ActiveLink from "../Components/ActiveLink";
 const DisktopNav = () => {
     return (
-        <Box >
-            Disk
+        <Box p="1rem" boxShadow="md">
+
+            <Box>
+                <Center>
+                    <Heading>Wasfati</Heading>
+                </Center>
+
+            </Box>
+            <Box pt="2rem">
+                <Box>
+                    <Text fontWeight="semibold" color="#b2b2b2" p="0.5rem">Main Courses</Text>
+                </Box>
+
+                <ActiveLink href="/breakfast" type="primary" icon={BsEggFried}>
+                    <a>Breakfast</a>
+
+                </ActiveLink>
+
+                <ActiveLink href="/lunch" type="primary" icon={IoFastFoodOutline}>
+                    <a>Lunch</a>
+
+                </ActiveLink>
+                <ActiveLink href="/dinner" type="primary" icon={GiChickenOven}>
+                    <a>Dinner</a>
+
+                </ActiveLink>
+                <ActiveLink href="/brunch" type="primary" icon={GrToast}>
+                    <a>Brunch</a>
+
+                </ActiveLink>
+
+
+
+            </Box>
+            <Box pt="2rem">
+                <Box>
+                    <Text fontWeight="semibold" color="#b2b2b2" p="0.5rem">Deserts</Text>
+                </Box>
+
+                <Link>
+                    <Box p="0.5rem">
+                        <Flex align="center">
+
+                            <Icon as={BsEggFried} color="black"></Icon>
+                            <Box ml="0.5rem">
+                                <Text color="black">Hot Deserts</Text>
+                            </Box>
+
+
+
+
+                        </Flex>
+                    </Box>
+                </Link>
+                <Link>
+                    <Box borderRadius="3xl" p="0.5rem">
+                        <Flex align="center">
+
+                            <Icon as={BsEggFried} color="black"></Icon>
+                            <Box ml="0.5rem">
+                                <Text color="black">Cold Deserts</Text>
+                            </Box>
+
+
+
+
+                        </Flex>
+                    </Box>
+                </Link>
+
+
+
+            </Box>
+            <Box pt="2rem">
+                <Box>
+                    <Text fontWeight="semibold" color="#b2b2b2" p="0.5rem">Drinks</Text>
+                </Box>
+
+                <Link>
+                    <Box p="0.5rem">
+                        <Flex align="center">
+
+                            <Icon as={BsEggFried} color="black"></Icon>
+                            <Box ml="0.5rem">
+                                <Text color="black">Coffee</Text>
+                            </Box>
+
+
+
+
+                        </Flex>
+                    </Box>
+                </Link>
+                <Link>
+                    <Box borderRadius="3xl" p="0.5rem">
+                        <Flex align="center">
+
+                            <Icon as={BsEggFried} color="black"></Icon>
+                            <Box ml="0.5rem">
+                                <Text color="black">Juice</Text>
+                            </Box>
+
+
+
+
+                        </Flex>
+                    </Box>
+                </Link>
+                <Link>
+                    <Box borderRadius="3xl" p="0.5rem">
+                        <Flex align="center">
+
+                            <Icon as={BsEggFried} color="black"></Icon>
+                            <Box ml="0.5rem">
+                                <Text color="black">Party Drinks</Text>
+                            </Box>
+
+
+
+
+                        </Flex>
+                    </Box>
+                </Link>
+
+
+
+            </Box>
+
+
         </Box>
     )
 }
